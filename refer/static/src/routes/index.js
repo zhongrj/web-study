7807674485e3db8efec03cc5dd14e52ca25609a2
rujie.zhong <329053269@qq.com>
2017-06-09 19:34:02 +0800

    修改

--- refer/static/src/routes/index.js
+++ refer/static/src/routes/index.js
@@ -1,20 +1,23 @@
 
 export default {
-    childRoutes: [{
-        path: 'login',
-        component: require('../components/Login/Login')
-    }, {
-        path: '/',
-        indexRoute: { onEnter: (nextState, replace) => replace('/polling') },
-        component: require('../components/App'),
-        getChildRoutes( partialNextState, callback ) {
-            require.ensure( [], function(require) {
-                callback(null, [
-                    require('../components/Polling'),
-                    require('../components/Rules'),
-                    require('../components/Password'),
-                ]);
-            } );
+    childRoutes: [
+        {
+            path: 'login',
+            component: require('../components/Login/Login')
+        },
+        {
+            path: '/',
+            indexRoute: { onEnter: (nextState, replace) => replace('/polling') },
+            component: require('../components/App'),
+            getChildRoutes( partialNextState, callback ) {
+                require.ensure( [], function(require) {
+                    callback(null, [
+                        require('../components/Polling'),
+                        require('../components/Rules'),
+                        require('../components/Password'),
+                    ]);
+                } );
+            }
         }
-    }]
+    ]
 };
